325a2b2d60554ceb4f86921d1fe2fa5603338698
markkimjr <markkimjr96@gmail.com>
2024-06-06 19:09:00 +0900

    moved base index route from routes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import traceback
+from flask import Flask, Response
 
 from log import Logger
 
@@ -6,6 +7,15 @@ log = Logger.get_instance()
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
+
+
+@app.route("/", methods=["GET"])
+def render_index() -> Response:
+    try:
+        return "Saved by the bell"
+    except Exception as e:
+        traceback.print_exc()
+        log.error(f"ERROR OCCURRED WHILE RENDERING INDEX")
 
 
 if __name__ == "__main__":
